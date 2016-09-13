3f2f165ac224d63ae1213200f9db5af23c6a5ec4
dibyendu <dibyendumondal14@gmail.com>
2016-09-13 19:43:26 +0530

    added pcl and thea demos

---
+++ Demo/PCL_Register/build/CMakeFiles/register.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/register.dir/register.cpp.o"
+  "register.pdb"
+  "register"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/register.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Demo/PCL_Register/build/allapply.sh
@@ -0,0 +1,5 @@
+for i in {0..14}
+do 
+./trimmer inputCloud_anand$i\_t1.pcd anand$i\_t1.pcd 
+# pcl_viewer inputCloud_anand$i_t.pcd
+done
